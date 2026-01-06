6b7ef33fdae05b6d59177a58fb941cbe03b2a31e
chanu <chanuka.williams@outlook.com>
2026-01-06 21:31:47 +0000

    Checks if token is still default value.

--- Program.cs
+++ Program.cs
@@ -5,13 +5,21 @@ namespace STRSTeamsCheckIn
     {
         private static void CreateEnvFile()
         {
-            File.WriteAllText("./.env", "TOKEN=YOUR_TOKEN");
+            File.WriteAllText("./.env", "TOKEN=PASTE_YOUR_TOKEN");
             Console.WriteLine("Created env file. Please add your token.");
         }
         private static void Main()
         {
             if (!File.Exists("./.env"))
+            {
                 CreateEnvFile();
+                return;
+            }
+            else if (File.ReadAllText("./.env") == "TOKEN=PASTE_YOUR_TOKEN")
+            {
+                Console.WriteLine("Token is still its default value. Please add your token.");
+                return;
+            }
             
         }
     }
