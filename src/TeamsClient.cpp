c925e7e68007ef0e97b940f243a2c2841483c519
chanu <chanuka.williams@outlook.com>
2026-02-09 14:43:12 +0000

    Using external library for base64 encoding.

--- src/TeamsClient.cpp
+++ src/TeamsClient.cpp
@@ -1,10 +1,11 @@
 #include "TeamsClient.h"
 
 #include <algorithm>
-#include <cstdint>
 #include <curl/curl.h>
 #include <iostream>
 #include <utility>
+
+#include "base64.hpp"
 
 #include "curl_wrappers/CurlHandle.h"
 #include "curl_wrappers/CurlHeaders.h"
@@ -16,7 +17,7 @@ TeamsClient::TeamsClient(std::string token) : m_token(std::move(token))
 Response TeamsClient::CheckIn(const std::string& location) const
 {
     const std::string baseUrl = "https://teamsapps.strschool.co.uk/api/touchreg/mobileCheckIn/";
-    std::string locationCode = ToBase64(location);
+    std::string locationCode = base64::to_base64(location);
 
     std::ranges::replace(locationCode, '+', '-');
     std::ranges::replace(locationCode, '/', '_');
@@ -59,45 +60,6 @@ Response TeamsClient::CheckIn(const std::string& location) const
     curl_easy_getinfo(curl.Get(), CURLINFO_RESPONSE_CODE, &statusCode);
 
     return Response{statusCode, responseBody};
-}
-
-std::string TeamsClient::ToBase64(const std::string& input)
-{
-    static constexpr char kBase64Table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-    std::string output;
-    output.reserve(((input.size() + 2) / 3) * 4);
-
-    size_t i = 0;
-    while (i < input.size())
-    {
-        std::uint32_t triple = 0;
-        int bytes_collected = 0;
-
-        for (int j = 0; j < 3; ++j)
-        {
-            triple <<= 8;
-            if (i < input.size())
-            {
-                triple |= static_cast<unsigned char>(input[i++]);
-                ++bytes_collected;
-            }
-        }
-
-        for (int j = 0; j < 4; ++j)
-        {
-            if (j <= bytes_collected)
-            {
-                output.push_back(kBase64Table[(triple >> (18 - j * 6)) & 0x3F]);
-            }
-            else
-            {
-                output.push_back('=');
-            }
-        }
-    }
-
-    return output;
 }
 
 size_t TeamsClient::WriteCallback(void* data, const size_t elementSize, const size_t elementCount, void* userContext)
